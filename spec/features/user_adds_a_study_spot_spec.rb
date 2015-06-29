8d755a26856da6f5f775b270aa0bc1a0afc1fb67
Twelvvy <lamonte.fields@gmail.com>
2015-06-29 15:30:03 -0400

    Add feature test

---
+++ spec/features/user_adds_a_study_spot_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature "adding spot" do
+  scenario "users add a new spot" do
+    visit "/study_spots/new"
+
+    fill_in "Name", with: "Launch Academy"
+    fill_in "Address", with: "33 Harrison Ave"
+    fill_in "City", with: "Boston"
+    select "MA", from: "State"
+    fill_in "Zip code", with: "03032"
+    fill_in "Website url", with: "http://pinballwizard.com"
+    fill_in "Photo url", with: "http://www.bootcamps.in/wp-content/uploads/2013/01/Launch_Academy_Logo.jpg"
+    fill_in "Phone", with: 6173659985
+    fill_in "Description", with: "This is a super chill, totally rad-tacular place to study"
+    fill_in "Type", with: "Shared workspace"
+    choose "Free Wi-Fi"
+    select "lots of outlets", from: "Outlets"
+    select "high", from: "Noise level"
+    select "comfy", from: "Comfort level"
+
+    click_button "Add new spot"
+
+    expect(page).to have_content("Study Spot Added")
+  end
+end
