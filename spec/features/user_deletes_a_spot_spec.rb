5ad79a7961aeb9dcb4b83a7d50b78d148519e14e
Twelvvy <lamonte.fields@gmail.com>
2015-07-02 16:16:41 -0400

    create edit function and test

---
+++ spec/features/user_deletes_a_spot_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "User deletes a spot" do
+
+# As a user I want to be able to edit a spot I created
+# so that the information is up-to-date.
+# Acceptance criteria:
+# User can edit the spot they created.
+
+  scenario 'I want to delete a study spot' do
+    spot = FactoryGirl.create(:spot)
+    visit spot_path(spot)
+
+    expect(page).to have_content("33 Harrison Ave")
+    expect(page).to have_content("Launch Academy")
+  end
+end
