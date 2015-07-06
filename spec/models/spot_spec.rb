bd63a2406f7f6bbae41f8774954d6e4f1c400cb2
Twelvvy <lamonte.fields@gmail.com>
2015-07-06 11:27:07 -0400

    Hound Comments

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -12,27 +12,27 @@ describe Spot do
   it { should validate_length_of(:zip_code) }
 end
 
-  # it "has a valid factory" do
-  #   FactoryGirl.build(:spot).should be_valid
-  # end
-  # it "is invalid without name" do
-  #   FactoryGirl.build(:spot, name: nil).should_not be_valid
-  # end
-  # it "is invalid without description" do
-  #   FactoryGirl.build(:spot, description: nil).should_not be_valid
-  # end
-  # it "is invalid without category" do
-  #   FactoryGirl.build(:spot, category: nil).should_not be_valid
-  # end
-  # it "is invalid without address" do
-  #   FactoryGirl.build(:spot, address: nil).should_not be_valid
-  # end
-  # it "is invalid without city" do
-  #   FactoryGirl.build(:spot, city: nil).should_not be_valid
-  # end
-  # it "is invalid without state" do
-  #   FactoryGirl.build(:spot, state: nil).should_not be_valid
-  # end
-  # it "is invalid without zip code" do
-  #   FactoryGirl.build(:spot, zip_code: nil).should_not be_valid
-  # end
+# it "has a valid factory" do
+#   FactoryGirl.build(:spot).should be_valid
+# end
+# it "is invalid without name" do
+#   FactoryGirl.build(:spot, name: nil).should_not be_valid
+# end
+# it "is invalid without description" do
+#   FactoryGirl.build(:spot, description: nil).should_not be_valid
+# end
+# it "is invalid without category" do
+#   FactoryGirl.build(:spot, category: nil).should_not be_valid
+# end
+# it "is invalid without address" do
+#   FactoryGirl.build(:spot, address: nil).should_not be_valid
+# end
+# it "is invalid without city" do
+#   FactoryGirl.build(:spot, city: nil).should_not be_valid
+# end
+# it "is invalid without state" do
+#   FactoryGirl.build(:spot, state: nil).should_not be_valid
+# end
+# it "is invalid without zip code" do
+#   FactoryGirl.build(:spot, zip_code: nil).should_not be_valid
+# end
