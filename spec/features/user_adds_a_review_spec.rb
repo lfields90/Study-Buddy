ead387e6e3942102865fbc57b78496ab34a111ee
Twelvvy <lamonte.fields@gmail.com>
2015-07-06 12:12:36 -0400

    Add before each into tests

--- spec/features/user_adds_a_review_spec.rb
+++ spec/features/user_adds_a_review_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 feature "adding review" do
-  scenario "users add a new review" do
+  before :each do
     user = FactoryGirl.create(:user)
     spot = FactoryGirl.create(:spot)
 
@@ -13,57 +13,31 @@ feature "adding review" do
 
     visit spot_path(spot)
     click_link("Add a review")
+  end
+
+  scenario "users add a new review" do
 
     choose "5"
     fill_in "Body", with: "Great study spot"
-
     click_button "Add Review"
 
     expect(page).to have_content("Review successfully added")
     expect(page).to have_content("Great study spot")
   end
 
-  feature "adding review" do
-    scenario "users add a new review with no rating" do
-      user = FactoryGirl.create(:user)
-      spot = FactoryGirl.create(:spot)
+  scenario "users add a new review with no rating" do
 
-      visit new_user_session_path
+    fill_in "Body", with: "Great study spot"
+    click_button "Add Review"
 
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-      click_button 'Log in'
-
-      visit spot_path(spot)
-      click_link("Add a review")
-
-      fill_in "Body", with: "Great study spot"
-
-      click_button "Add Review"
-
-      expect(page).to have_content("Rating can't be blank")
-    end
+    expect(page).to have_content("Rating can't be blank")
   end
 
-  feature "adding review" do
-    scenario "users add a new review with no rating" do
-      user = FactoryGirl.create(:user)
-      spot = FactoryGirl.create(:spot)
+  scenario "users add a new review with no rating" do
 
-      visit new_user_session_path
+    choose "3"
+    click_button "Add Review"
 
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-      click_button 'Log in'
-
-      visit spot_path(spot)
-      click_link("Add a review")
-
-      choose "3"
-
-      click_button "Add Review"
-
-      expect(page).to have_content("Body can't be blank")
-    end
+    expect(page).to have_content("Body can't be blank")
   end
 end
