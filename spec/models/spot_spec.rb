65208315cc1f84a3c2601c72cba7dc216f5f6931
Twelvvy <lamonte.fields@gmail.com>
2015-07-06 12:27:17 -0400

    Update tests ! '

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -11,28 +11,3 @@ describe Spot do
   it { should validate_presence_of(:zip_code) }
   it { should validate_length_of(:zip_code) }
 end
-
-# it "has a valid factory" do
-#   FactoryGirl.build(:spot).should be_valid
-# end
-# it "is invalid without name" do
-#   FactoryGirl.build(:spot, name: nil).should_not be_valid
-# end
-# it "is invalid without description" do
-#   FactoryGirl.build(:spot, description: nil).should_not be_valid
-# end
-# it "is invalid without category" do
-#   FactoryGirl.build(:spot, category: nil).should_not be_valid
-# end
-# it "is invalid without address" do
-#   FactoryGirl.build(:spot, address: nil).should_not be_valid
-# end
-# it "is invalid without city" do
-#   FactoryGirl.build(:spot, city: nil).should_not be_valid
-# end
-# it "is invalid without state" do
-#   FactoryGirl.build(:spot, state: nil).should_not be_valid
-# end
-# it "is invalid without zip code" do
-#   FactoryGirl.build(:spot, zip_code: nil).should_not be_valid
-# end
