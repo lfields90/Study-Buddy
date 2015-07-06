42c3f3bfde7e1c7e0eb6cc357b3df9169eb29dbc
Twelvvy <lamonte.fields@gmail.com>
2015-07-06 10:54:24 -0400

    Add tests for adding a review

---
+++ spec/features/user_adds_a_review_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+feature "adding review" do
+  scenario "users add a new review" do
+    user = FactoryGirl.create(:user)
+    spot = FactoryGirl.create(:spot)
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
+    visit spot_path(spot)
+    click_link("Add a review")
+
+    choose "5"
+    fill_in "Body", with: "Great study spot"
+
+    click_button "Add Review"
+
+    expect(page).to have_content("Review successfully added")
+    expect(page).to have_content("Great study spot")
+  end
+end
