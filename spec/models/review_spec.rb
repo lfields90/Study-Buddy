219594afe4363e4faad9abee1f609b3494731e5c
Twelvvy <lamonte.fields@gmail.com>
2015-07-06 11:23:02 -0400

    Update test suite

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 describe Review do
   it { should validate_presence_of(:body) }
   it { should validate_presence_of(:rating) }
+  it { should validate_numericality_of(:rating) }
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:spot) }
 end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -9,6 +9,7 @@ describe Spot do
   it { should validate_presence_of(:city) }
   it { should validate_presence_of(:state) }
   it { should validate_presence_of(:zip_code) }
+  it { should validate_length_of(:zip_code) }
 end
 
   # it "has a valid factory" do
