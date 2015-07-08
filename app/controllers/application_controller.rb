86f212997b980cdd9ad32d41643968ef3714bd7a
Nora Delaney <nora.delaney@gmail.com>
2015-07-08 11:02:03 -0400

    hound issues...again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) << :username
-   devise_parameter_sanitizer.for(:sign_up) << :first_name
-   devise_parameter_sanitizer.for(:sign_up) << :last_name
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
   end
 end
