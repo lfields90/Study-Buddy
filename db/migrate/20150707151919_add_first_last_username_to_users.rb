df52131b16b3ac1abb93cbf115758f1a70aa8137
Nora Delaney <nora.delaney@gmail.com>
2015-07-10 15:29:14 -0400

    fix migration name

--- db/migrate/20150707151919_add_first_last_username_to_users.rb
+++ db/migrate/20150707151919_add_first_last_username_to_users.rb
@@ -1,4 +1,4 @@
-class AddFirstUsernameToUsers < ActiveRecord::Migration
+class AddFirstLastUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :first_name, :string, null: false
     add_column :users, :last_name, :string, null: false
